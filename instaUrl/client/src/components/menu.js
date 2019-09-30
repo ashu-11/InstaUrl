b7b53f770efd95974ecf7602bc8ca5ca13ca2d4b
Ashutosh Mishra <ashutosh.shai84@gmail.com>
2019-09-30 08:52:02 +0530

    Update menu.js

--- instaUrl/client/src/components/menu.js
+++ instaUrl/client/src/components/menu.js
@@ -1,12 +1,13 @@
 import React  from 'react';
 import {Nav,Navbar} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logonav.png';
 
 class Menu extends React.Component {
    render() {
        return(
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#/">Instacar</Navbar.Brand>
+                <Navbar.Brand href="#/"><img   style={{width:100, marginTop: -7}}   src={logo }/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mr-auto">
